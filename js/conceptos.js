56a9463b621c5917cf2a47f29be03d5ef5ea2fbd
jorgegrajalesu <jegosystem@gmail.com>
2022-11-17 14:06:38 -0500

    vjs171122

---
+++ js/conceptos.js
@@ -0,0 +1,16 @@
+// concetos básicos en javascript
+/* 
+declarar variables en javascript:
+1. var
+2. let
+3. const
+*/ 
+
+var nombre = "jorge";//vble de tipo cadena o texto
+let edad = 45;//vble de tipo entero
+const iva = 0.19; //vble de tipo flotante
+
+//datos de salida o imprimir un dato en la pagina web
+document.write(nombre);
+//console.log en la consola del navegador o devtools
+console.log(nombre);
