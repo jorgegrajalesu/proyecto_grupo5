acfc067883bc38b03ac572c5e129f11d38125b72
jorgegrajalesu <jegosystem@gmail.com>
2022-11-22 14:05:48 -0500

    v221122

---
+++ js/factura.js
@@ -0,0 +1,7 @@
+//crear una función
+function calcular(){
+    //definir variables para el iva, precio y cantidad
+    const iva = 0.19;
+    let precio = document.simuladorFactura.precio.value;
+
+}
